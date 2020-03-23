1569efa5918069c16c4c530ebddb1be88c398917
Saulo Soares <saulosoares150498@gmail.com>
2020-03-23 17:35:10 -0300

    Initial commit

---
+++ Source/BattleTankUdemy/BattleTankUdemyGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BattleTankUdemyGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANKUDEMY_API ABattleTankUdemyGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
