891ccd081222beee63ae0975c036d928e02c5a5e
Saulo Soares <saulosoares150498@gmail.com>
2020-04-20 20:59:48 -0300

    LE222_BlueprintSpawnableComponent

--- Source/BattleTankUdemy/TankBarrel.h
+++ Source/BattleTankUdemy/TankBarrel.h
@@ -9,11 +9,18 @@
 /**
  * 
  */
-UCLASS()
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent), HideCategories = ("Collision"))
 class BATTLETANKUDEMY_API UTankBarrel : public UStaticMeshComponent
 {
 	GENERATED_BODY()
 public:
 	void Elevate(float DegreesPerSecond);
+private:
+	UPROPERTY(EditAnywhere, Category = Setup)
+	float DegreePerSeconds = 20.f;
+	UPROPERTY(EditAnywhere, Category = Setup)
+	float MinElevationDegree = 0.f;
+	UPROPERTY(EditAnywhere, Category = Setup)
+	float MaxElevationDegree = 40.f;
 	
 };
