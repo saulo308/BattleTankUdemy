24e5b9a63e913d71793477a268feecf286c0f782
Saulo Soares <saulosoares150498@gmail.com>
2020-04-26 17:47:01 -0300

    LE247_CrossProduct AI Turn right

--- Source/BattleTankUdemy/TankMovementComponent.cpp
+++ Source/BattleTankUdemy/TankMovementComponent.cpp
@@ -20,12 +20,20 @@ void UTankMovementComponent::RequestDirectMove(const FVector& MoveVelocity, bool
     //Caculating the "paralelness" of tank forward vector and the vector where the AI wants to go
     auto TankForward = GetOwner()->GetActorForwardVector().GetSafeNormal();
     auto AIForwardIntention = MoveVelocity.GetSafeNormal();
+
     //We're using a dot product to calculate how "right" the forward vector is of where the AI wants to go
     //(1 = TankForward = AIForwardIntention; -1 = TankForward = -AIForwardIntention; 0 if TankForward and AIForwardIntention are perpendicular)
     auto ForwardThrow = FVector::DotProduct(TankForward,AIForwardIntention);
-
+    
     //Calling Move forward
     IntendMoveForward(ForwardThrow);
+
+    //We're using cross product to calculate how "right" the forward vector if of where the AI wans to rotate
+    //(1 if TankForward and AIForwardRotation are perpendiculars;0 if TankForward = AIForwardIntention)
+    auto RotateThrow = FVector::CrossProduct(TankForward,AIForwardIntention).Z;
+
+    //Calling rotate
+    IntendTurnRight(RotateThrow);
 }
 
 void UTankMovementComponent::IntendMoveForward(float Throw){
@@ -36,6 +44,7 @@ void UTankMovementComponent::IntendMoveForward(float Throw){
 
 void UTankMovementComponent::IntendTurnRight(float Throw){
     if(!LeftTrack || !RightTrack) return;
+
     LeftTrack->SetThrottle(Throw);
     RightTrack->SetThrottle(-Throw);    
 }
